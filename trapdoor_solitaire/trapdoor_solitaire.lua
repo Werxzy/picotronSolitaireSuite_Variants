2736181d24a93d2dae7ad0123a6752d032f80bdc
Werxzy <Werxzyz@yahoo.com>
2024-06-10 05:39:39 -0500

    Updated card generation to newest version.

--- trapdoor_solitaire/trapdoor_solitaire.lua
+++ trapdoor_solitaire/trapdoor_solitaire.lua
@@ -1,4 +1,4 @@
---[[pod_format="raw",created="2024-03-17 19:21:13",modified="2024-06-04 04:54:30",revision=3329]]
+--[[pod_format="raw",created="2024-03-17 19:21:13",modified="2024-06-10 10:39:09",revision=3434]]
 
 
 include "suite_scripts/rolling_score.lua"
@@ -42,11 +42,23 @@ function game_setup()
 		wins = 0
 	}	
 	
-	local card_sprites = card_gen_standard(4, 10, nil, all_ranks, nil, all_face_sprites)
+	local card_back = suite_card_back()
+	
+	local card_sprites = card_gen_standard({
+		suits = 4,
+		ranks = 10,
+		rank_chars = all_ranks,
+		face_sprites = all_face_sprites
+	})
 	
 	for suit = 1,available_suits do
 		for rank = 1,#all_ranks do		
-			local c = card_new(card_sprites[suit][min(rank, 10)], 240,100)
+			local c = card_new({
+				sprite = card_sprites[suit][min(rank, 10)], 
+				back_sprite = card_back,
+				x = 240,
+				y = 100
+			})
 			c.suit = suit
 			c.rank = rank
 		end
